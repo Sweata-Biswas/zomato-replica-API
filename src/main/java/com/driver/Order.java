ded5b70805eceb65994c9c247abea47bf6c5ccf5
sweata Biswas <115707450+Sweata-Biswas@users.noreply.github.com>
2023-01-23 17:43:29 +0530

    First commit

---
+++ src/main/java/com/driver/Order.java
@@ -0,0 +1,32 @@
+package com.driver;
+
+public class Order {
+
+    private String id;
+    private int deliveryTime;
+
+    public Order(String id, String deliveryTime) {
+
+        // The deliveryTime has to converted from string to int and then stored in the attribute
+        //deliveryTime  = HH*60 + MM
+
+        this.deliveryTime = Integer.parseInt(deliveryTime.substring(0,2))*60 + Integer.parseInt(deliveryTime.substring(3,5));
+        this.id = id;
+        
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setDeliveryTime(String deliveryTime) {
+        this.deliveryTime = Integer.parseInt(deliveryTime.substring(0,2))*60 + Integer.parseInt(deliveryTime.substring(3,5));
+    }
+
+    public int getDeliveryTime() {return deliveryTime;}
+}
